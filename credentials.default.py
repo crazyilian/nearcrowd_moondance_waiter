ac7e846aebb8b018b7b515c5a410f19296dbab58
Ilian Andrianov <ilian.and.04@gmail.com>
2022-04-18 11:38:07 +0300

    [feat]: new example in default credentials

--- credentials.default.py
+++ credentials.default.py
@@ -1,8 +1,10 @@
 PRIVATE_KEY = "ed25519:XXXX"
 ACCOUNT_NAME = "XXX.near"
 
-
+# import sys
+#
 # ACCOUNT_NUMBER = 0
+# ACCOUNT_NUMBER = int(sys.argv[1])  # "> python claim_task.py k" will run under k-th user
 #
 # match ACCOUNT_NUMBER:
 #     case 0:
@@ -11,3 +13,4 @@ ACCOUNT_NAME = "XXX.near"
 #     case 1:
 #         ACCOUNT_NAME = "account2.near"
 #         PRIVATE_KEY = "ed25519:YYYY"
+
